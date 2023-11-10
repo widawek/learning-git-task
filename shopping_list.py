e6a6f5db2067067d5e1bbf34a301d82eb1268862
widawek <kolczywiewicz@gmail.com>
2023-11-10 15:35:34 +0100

    Add text generator

--- shopping_list.py
+++ shopping_list.py
@@ -3,3 +3,11 @@ shopping = {
     'piekarnia': ['chleb', 'bułki', 'pączek'],
     'warzywniak': ['marchew', 'seler', 'rukola']
     }
+
+
+text = ''
+for key, value in shopping.items():
+    key = key
+    text += f'Idę do {key}, kupuję tu następujące rzeczy: {value}\n'
+
+print(text)
