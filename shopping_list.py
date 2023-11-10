33e3ac6389c62d0830786638ed09259c258679c4
widawek <kolczywiewicz@gmail.com>
2023-11-10 15:40:02 +0100

    Add capitalize function for shops and products

--- shopping_list.py
+++ shopping_list.py
@@ -7,7 +7,8 @@ shopping = {
 
 text = ''
 for key, value in shopping.items():
-    key = key
+    key = key.capitalize()
+    value = [i.capitalize() for i in value]
     text += f'Idę do {key}, kupuję tu następujące rzeczy: {value}\n'
 
 print(text)
